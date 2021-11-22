bb289a082ce26527492591d892b2a5bdb432d71c
Thriliriel <wsk.shadow@gmail.com>
2021-11-22 13:29:41 -0300

    finally

---
+++ clearMemory.py
@@ -0,0 +1,10 @@
+﻿import os
+
+if os.path.exists("nameFile.npy"):
+	os.remove("nameFile.npy")
+if os.path.exists("faceFile.npy"):
+	os.remove("nameFile.npy")
+
+dir = 'Data'
+for f in os.listdir(dir):
+    os.remove(os.path.join(dir, f))
